f830cba014b489579d77c508b0536f5a9466fde5
CARLOS FERNANDES <carlos2.fernandes@usp.br>
2020-09-15 14:24:42 -0300

    added a form to index for image upload and processing options; added a POST request handler from the form to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import (Flask, render_template)
+from flask import (Flask, render_template, request, redirect, send_file, url_for)
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -19,10 +19,24 @@ def create_app(test_config=None):
     except OSError:
         pass   
 
+    app.config["IMAGE_UPLOADS"] = os.path.join(os.path.abspath(os.path.dirname("")), 'static')
+
     # == routes ==
     @app.route('/', methods=['GET', 'POST'])
     def index():
         
+        if request.method == 'POST':
+
+            if request.files:
+                options = request.form["options"]
+                image = request.files["image"]
+                path = os.path.join(app.config["IMAGE_UPLOADS"], image.filename)
+                print(options)
+                print(path)
+                print(image)
+                image.save(path)
+                return redirect(url_for('index'))
+
         return render_template('/index.html')
 
     return app
